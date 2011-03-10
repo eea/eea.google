1830f6b311ae4b71240ebc3540ec9643c32fb232
Alin Voinea <contact@avoinea.com>
2011-03-10 17:07:11 +0000

    - Fix import.

--- eea/google/tool/google.py
+++ eea/google/tool/google.py
@@ -1,5 +1,5 @@
 from zope.interface import implements
-from eea.google.analytics.interfaces import IGoogleTool
+from eea.google.tool.interfaces import IGoogleTool
 from OFS.Folder import Folder
 
 class GoogleTool(Folder):
