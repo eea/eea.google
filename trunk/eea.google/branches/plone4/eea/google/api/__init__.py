59a5ccd0bb7a2356c8200b2f78825ae529427d6e
voineali <voineali@24821446-e7ea-0310-81ac-b189f4507b0b>
2011-04-14 13:37:32 +0000

    - Pylint, pyflakes.

--- trunk/eea.google/branches/plone4/eea/google/api/__init__.py
+++ trunk/eea.google/branches/plone4/eea/google/api/__init__.py
@@ -2,4 +2,8 @@
 
 from eea.google.api.connection import Connection
 from eea.google.api.exception import GoogleClientError
-all_ = [Connection.__name__, GoogleClientError.__name__]
+
+__all__ = [
+    Connection.__name__,
+    GoogleClientError.__name__,
+]
